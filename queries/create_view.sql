d163f76e997dd7cbae76fdf05414599ae1788501
WhiteW00LF <mocha_cookie_crumble@proton.me>
2025-07-19 10:02:22 +0530

    updated create_view

--- queries/create_view.sql
+++ queries/create_view.sql
@@ -7,7 +7,12 @@ op.payment_type AS payment_type,
 op.payment_installments AS installments, 
 op.payment_value AS price_paid,
 orv.review_score AS review_score,
-orv.review_creation_date AS review_date
+orv.review_creation_date AS review_date,
+s.seller_id AS seller_id,
+s.seller_city AS seller_city,
+s.seller_state AS seller_state,
+p.product_id AS product_id,
+p.product_category_name AS product_category
 FROM order_items oi 
 INNER JOIN order_payments op 
 ON oi.order_id = op.order_id
