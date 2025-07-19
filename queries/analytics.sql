32248494e0776933455f22dcc6de1f0e4f302f9a
WhiteW00LF <mocha_cookie_crumble@proton.me>
2025-07-19 10:19:51 +0530

    added code for 3 questions

--- queries/analytics.sql
+++ queries/analytics.sql
@@ -1,10 +1,21 @@
 
--- Order by price_paid descending and limit to top 10 purchases
+--How many unique customers do you have?
 
-SELECT * FROM 
-purchase_table
-ORDER BY price_paid DESC
-LIMIT 10;
+SELECT COUNT(customer_unique_id) AS customers FROM purchase_table
+;
+
+--Which 5 cities have placed the most orders?
+
+SELECT COUNT(order_id) AS orders, customer_city FROM purchase_table 
+GROUP BY customer_city
+ORDER BY orders DESC
+LIMIT 5;
+
+--What are the different order statuses and how many orders fall into each status?
+
+
+SELECT order_status,COUNT(order_id) AS orders FROM orders
+GROUP BY order_status
 
 
 
